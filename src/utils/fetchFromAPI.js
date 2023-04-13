50deb4571abe668381c0a65668eccdaeccc2cfd3
Rebecca Martins <61643695+mtrebecca@users.noreply.github.com>
2023-04-13 19:01:40 -0300

    initial project

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '32d3da625bmsh891f2e6c112725ap1f46aejsn0409d80b6b63',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  console.log(data);
+  return data;
+};
